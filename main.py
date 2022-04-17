b322f827094f782b675ffb48f960ef2a9196504a
davebecoding <d01001010@csu.fullerton.edu>
2022-04-17 10:34:05 -0700

    stage files and docs

---
+++ main.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+# connecting to the database
+connection = sqlite3.connect("test.db")
+
+# cursor
+crsr = connection.cursor()
+
+# print statement will execute if there
+# are no errors
+print("Connected to the database")
+
+# close the connection
+connection.close()
+
