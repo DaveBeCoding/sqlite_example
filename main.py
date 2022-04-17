f04069fa0ffb13ab8991ec22842e89cd1af86734
davebecoding <d01001010@csu.fullerton.edu>
2022-04-17 11:10:51 -0700

    deleting row from table, WHERE fname Rishabh

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ conn = sqlite3.connect('test.db')
 cursor = conn.cursor()
 
 # Updating
-cursor.execute('''UPDATE emp SET lname = "Jyoti" WHERE fname="Rishabh";''')
-
+cursor.execute('''DELETE FROM emp WHERE fname="Rishabh";''')
+  
 # Commit your changes in the database
 conn.commit()
-
+  
 # Closing the connection
 conn.close()
