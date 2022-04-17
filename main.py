db76d2fd57c4225902f71f04cd1ec9b30de83e97
davebecoding <d01001010@csu.fullerton.edu>
2022-04-17 10:37:48 -0700

    stage empty table

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ connection = sqlite3.connect("test.db")
 # cursor
 crsr = connection.cursor()
 
-# print statement will execute if there
-# are no errors
-print("Connected to the database")
-
+# SQL command to create a table in the database
+sql_command = """CREATE TABLE emp ( 
+staff_number INTEGER PRIMARY KEY, 
+fname VARCHAR(20), 
+lname VARCHAR(30), 
+gender CHAR(1), 
+joining DATE);"""
+  
+# execute the statement
+crsr.execute(sql_command)
+  
 # close the connection
 connection.close()
-
